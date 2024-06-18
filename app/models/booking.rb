63f0c4a9c6a0f7f843067c9f12159ccb489a1f78
Paul Portier <paulportier@gmail.com>
2024-06-18 23:24:52 +0200

    add reservation+booking missions callbacks

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,23 @@ class Booking < ApplicationRecord
   belongs_to :listing
 
   validates :start_date, :end_date, presence: true
+
+  after_create :create_missions
+
+  private
+
+  def create_missions
+    listing.missions.create([
+      {
+        date: start_date,
+        mission_type: :first_checkin,
+        price: 10 * listing.num_rooms
+      },
+      {
+        date: end_date,
+        mission_type: :last_checkout,
+        price: 5 * listing.num_rooms
+      }
+    ])
+  end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,4 +5,18 @@ class Reservation < ApplicationRecord
   belongs_to :listing
 
   validates :start_date, :end_date, presence: true
+
+  after_create :create_missions
+
+  private
+
+  def create_missions
+    return if listing.missions.exists?(date: end_date, mission_type: :last_checkout)
+
+    listing.missions.create(
+      date: end_date,
+      mission_type: :checkout_checkin,
+      price: 10 * listing.num_rooms
+    )
+  end
 end
